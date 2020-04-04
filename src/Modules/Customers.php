796f60076036f39fc38cf9d02168adb875b284a5
Camoo Dev <etn@camoo.sarl>
2020-04-04 13:07:52 +0200

    Get Customer By Id/Email added

--- src/Modules/Customers.php
+++ src/Modules/Customers.php
@@ -8,8 +8,30 @@ namespace Camoo\Hosting\Modules;
  */
 class Customers extends AppModules
 {
+    /**
+     * @param array $data
+     * @return Response
+     */
     public function add($data)
     {
         return $this->client->post(\Camoo\Hosting\Lib\Client::API_ENDPOINT.'customers/add', $data);
+    }
+
+    /**
+     * @param string $email
+     * @return Response
+     */
+    public function getByEmail($email)
+    {
+        return $this->client->post(\Camoo\Hosting\Lib\Client::API_ENDPOINT.'customers/get-by-email/'. $mail);
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function getById($id)
+    {
+        return $this->client->post(\Camoo\Hosting\Lib\Client::API_ENDPOINT.'customers/get-by-id/'.$id);
     }
 }

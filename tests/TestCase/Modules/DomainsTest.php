953155fffd082c917da5aa055958b3ae3fc7a3fe
Epiphane Ndamzo <dev-team@camoo.sarl>
2026-09-13 12:11:21 +0200

    update

--- tests/TestCase/Modules/DomainsTest.php
+++ tests/TestCase/Modules/DomainsTest.php
@@ -3,73 +3,63 @@
 namespace CamooHosting\Test\TestCase\Modules;
 
 use Camoo\Hosting\Lib\Response;
+use Camoo\Hosting\Modules\Domains;
 use Camoo\Hosting\TestSuite\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * Class DomainsTest
  *
  * @author CamooSarl
- *
- * @covers \Camoo\Hosting\Modules\Domains
  */
+#[CoversClass(Domains::class)]
 class DomainsTest extends TestCase
 {
-    /**
-     * @covers \Camoo\Hosting\Modules\Domains::checkAvailability
-     *
-     * @testWith        ["test", "cm"]
-     * 					["camoo", "cm"]
-     */
-    public function testCheckAvailability($domain, $tld)
+    #[TestWith(["test", "cm"])]
+    #[TestWith(["camoo", "cm"])]
+    public function testCheckAvailability(string $domain, string $tld): void
     {
         $result = $this->oClientMocked->checkAvailability($domain, $tld);
         $this->assertInstanceOf(Response::class, $result);
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::register */
-    public function testRegister()
+    public function testRegister(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->register([]));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::renew */
-    public function testRenew()
+    public function testRenew(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->renew([]));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::suspend */
-    public function testSuspend()
+    public function testSuspend(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->suspend(111));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::unsuspend */
-    public function testUnSuspend()
+    public function testUnSuspend(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->unsuspend(11));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::resendVerificationMail */
-    public function testresendVerificationMail()
+    public function testresendVerificationMail(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->resendVerificationMail(11));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::isTransferable */
-    public function testisTranferable()
+    public function testisTranferable(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->isTransferable('test.cm'));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::cmWhois */
-    public function testcmWhois()
+    public function testcmWhois(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->cmWhois('test.cm'));
     }
 
-    /** @covers \Camoo\Hosting\Modules\Domains::transfer */
-    public function testTransfer()
+    public function testTransfer(): void
     {
         $this->assertInstanceOf(Response::class, $this->oClientMocked->transfer([]));
     }
